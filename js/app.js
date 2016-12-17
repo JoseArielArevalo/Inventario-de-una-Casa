bb37d3aa1d65fc20b55bf3c79b85566cca85ab8f
josearevalo <Jose.Arevalo@fundacion-jala.org>
2016-12-17 18:00:52 -0400

    obtener alerta de productos

--- js/app.js
+++ js/app.js
@@ -4,9 +4,17 @@ app.controller('misArticulos', function($scope, $http) {
   .then(function(response) {
       $scope.categoria = response.data;
   });
+})
+
+app.controller('misAlertas', function($scope, $http) {
+  $http.get("../php/productos_en_alerta.php")
+  .then(function(response) {
+      $scope.categoria = response.data;
+      var len = $scope.categoria.length;
+      console.log(len);
+      $scope.cantidad=len;      
+  });
 });
-
-
 
 
 
